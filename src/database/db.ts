a65edf7adfe2de03a4f0518ce9ef711abd1db3c4
Djin <32814285+jdnejal@users.noreply.github.com>
2023-06-15 23:41:41 +0200

    Database connection

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,6 +1,24 @@
 import mongoose from 'mongoose';
+import Question from './question';
 
-mongoose.set('strictQuery', false);
+console.log('TEST');
+
 mongoose.connect(
     'mongodb+srv://jayinnejal:gfRbC6H7C4hqycn4@cluster0.wv2fzug.mongodb.net/AK_DB/?retryWrites=true&w=majority'
 );
+
+// Create a new question post
+const question = new Question({
+    id: '123abc',
+    author: 'Jayin',
+    question: 'How to earn money quick?',
+    status: 1,
+    solution: 'Stock trading',
+});
+
+// Insert the article in MongoDB database
+question.save();
+
+// Find a single blog post
+const firstQuestion = Question.findOne({});
+console.log(firstQuestion);
