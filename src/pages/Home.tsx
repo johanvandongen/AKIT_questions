e17ae62201c1fac34f337bbe28e94080d74568f2
Djin <32814285+jdnejal@users.noreply.github.com>
2023-06-16 14:53:13 +0200

    database async function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import { TableView } from '../components/table/TableView';
+import { db } from '../database/db';
 
 export default function Home(): JSX.Element {
+    void db();
+
     return (
         <div>
             Home screen
