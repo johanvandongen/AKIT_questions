f5582ad66710096395f7cc37217c86f7f8284a2e
johanvandongen <74316747+johanvandongen@users.noreply.github.com>
2023-07-01 20:43:13 +0200

    add req body checks

--- server/schema/questionSchema.mjs
+++ server/schema/questionSchema.mjs
@@ -3,7 +3,8 @@ import { check } from "express-validator"
 const questionSchema = [
     check('question').notEmpty().withMessage('Questoin must not be empty'),
     check('author').notEmpty().withMessage('Author must not be empty'),
-    check('treated.state').notEmpty().withMessage('Treated must not be empty'),
+    check('treated.state').notEmpty().withMessage('Treated must not be empty').isIn(['Yes', 'No', 'Pending']).withMessage('Treated state must be one of the following: Yes, No or Pending'),
+    check('treated.remark').if((value, {req}) => {return req.body.treated.state === 'Pending'}).notEmpty().withMessage('A remark must be added when the question state is pending'),
     check('exerciseIds').isArray({min: 0}).withMessage('List of exercise ids must be included')
 ]
 
